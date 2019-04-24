5730add7dd61f3250567c53e70f8b290fd6c7e11
Heisler.Luke <Heisler.Luke@SEMS-259-27.SEMS259.LAB>
2019-04-24 16:24:28 -0700

    20190424

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.MalformedURLException;
@@ -50,31 +51,61 @@ public class SlotMachine implements Runnable, MouseListener {
 		//clear reels panel
 		imagePanel.removeAll();
 		//call SpinReel for each reel
-		spinReel();
+		int reel1 = spinReel();
+		int reel2 = spinReel();
+		int reel3 = spinReel();
 		//check for win
-		
+		if(reel1==reel2&&reel2==reel3) {
+			System.out.println("YEEEEEEEEEEE HAWWWWWWWWWWWWW! - that's a dollar and a penny");
+		}
+		else if(reel1==0&&reel2==0) {
+			System.out.println("yee haw!! - that's a dollar");
+		}
+		else if(reel1==0) {
+			System.out.println("yee haw - that's a penny");
+		}
 		//re-validate the reels panel
-		
+		imagePanel.revalidate();
+		frame.revalidate();
 	}
 	
 	private int spinReel() {
 		//get random value 0-2
 		Random random = new Random();
-		int randInt = random.nextInt(2);
+		int randInt = random.nextInt(100);
+		int returnInt = -1;
 		//load image based on random number
-		if(randInt==0) {
-			imagePanel.add(createLabelImage("Roblox.png"));
+		if(randInt>=0&&randInt<=49) {
+			try {
+				imagePanel.add(createLabelImage("Roblox.png"));
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			returnInt = 0;
 		}
-		else if(randInt==1) {
-			
+		else if(randInt>=50&&randInt<=74) {
+			try {
+				imagePanel.add(createLabelImage("Pokeball.PNG"));
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			returnInt = 1;
 		}
-		else if(randInt==2) {
-			
+		else if(randInt>=75&&randInt<=99) {
+			try {
+				imagePanel.add(createLabelImage("PiggyWig.png"));
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			returnInt = 2;
 		}
 		//add label to reels panel
 		
 		//return random number
-		return(randInt);
+		return(returnInt);
 	}
 	
 	private JLabel createLabelImage(String fileName) throws MalformedURLException{
@@ -92,7 +123,7 @@ public class SlotMachine implements Runnable, MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		if (arg0.getSource().equals(button)) {
-			System.out.println("Clicked!");
+			spinReels();
 		}
 	}
 
